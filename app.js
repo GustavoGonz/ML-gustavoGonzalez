5cc606f09e9ad2fb04d45fa901b4f395fd30e243
Gustavo <gustavodanielgb@gmail.com>
2024-06-27 20:53:07 -0300

    First commit: create the repository

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+
+const PORT = 3000;
+
+// Middleware para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static('public'));
+
+// Escuchar en el puerto definido
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+// Ruta Home
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "home.html"), (err) => {
+        if (err) {
+            console.error(`Error al enviar el archivo home.html: ${err}`);
+            res.status(err.status || 500).send('Error interno del servidor');
+        }
+    });
+});
+
+//Ruta Register
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "register.html"), (err) => {
+        if (err) {
+            console.error(`Error al enviar el archivo register.html: ${err}`);
+            res.status(err.status || 500).send('Error interno del servidor');
+        }
+    });
+});
+
+//Ruta Login
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "login.html"), (err) => {
+        if (err) {
+            console.error(`Error al enviar el archivo login.html: ${err}`);
+            res.status(err.status || 500).send('Error interno del servidor');
+        }
+    });
+});
